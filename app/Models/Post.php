20d8a7e09e2481997b624a45982ee4aaa04e5b21
bashir <bashirahmed.cse@gmail.com>
2023-02-03 23:21:44 +0600

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,6 +12,9 @@ class Post extends Model
     protected $fillable=[
      'title',
      'category_id',
+     'thumbnail',
+     'description',
+     'status',
     ];
 
     public function category()
